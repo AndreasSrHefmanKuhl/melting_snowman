2b9b8b37cbf16aea0785a29d44f026be46b7df6c
heavsmac <hefbolzen@gmail.com>
2025-11-04 09:25:00 +0100

    updated ascii for 3 more mistakes

--- ascii_art.py
+++ ascii_art.py
@@ -1,7 +1,10 @@
 
 
+# ascii_art.py
+
+# Stages of the Snowman Meltdown (6 stages total: 0 to 5)
 STAGES = [
-     # Stage 0: Full snowman
+     # Stage 0 (0 mistakes): Full snowman
      """
       ___  
      /___\\ 
@@ -9,20 +12,35 @@ STAGES = [
      ( : ) 
      ( : ) 
      """,
-     # Stage 1: Bottom part starts melting
+     # Stage 1 (1 mistake): Loss of a button
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     (   ) 
+     """,
+     # Stage 2 (2 mistakes): Loss of another button
      """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
      """,
-     # Stage 2: Only the head remains
+     # Stage 3 (3 mistakes): Bottom part starts melting
      """
       ___  
      /___\\ 
-     (o o) 
+     (o .) 
+     ( : ) 
      """,
-     # Stage 3: Snowman completely melted
+     # Stage 4 (4 mistakes): Only the head remains and is melting
+     """
+      ___  
+     /___\\ 
+     ( . ) 
+     """,
+     # Stage 5 (5 mistakes): Snowman completely melted (Loss condition)
      """
       ___  
      /___\\ 
